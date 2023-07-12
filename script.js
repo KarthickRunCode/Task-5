e372e74b9d662738ec211f849368bd55b15ef4d7
KarthickRunCode <karthickcoolpad@gmail.come>
2023-07-12 17:46:25 +0530

    complete

---
+++ script.js
@@ -0,0 +1,19 @@
+var resume=[
+    {
+        "Name":"karthick",
+        "Data birth":"14/03/2000",
+        "Age":"23",
+        "Gender":"male",
+        "Nationality":"Indian",
+        "Language Known":"Tamil,English",
+        "Married_status":"Single",
+        "10th mark percent":"87",
+        "12th mark percent":"59",
+        "B.E CGBA":"7.3",
+        "Work Experience":"0-1",
+       
+    }
+]
+for(var i=0;i<resume.length;i++){
+    console.log(resume[i]);
+}
